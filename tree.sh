c6e7645ebd916c5491957de1457521fbeb4fd6ac
RavelloH <ravellohh@gmail.com>
2022-08-04 18:36:30 +0800

    Generate source

--- tree.sh
+++ tree.sh
@@ -1,1 +1,8 @@
-echo "hello world"
+if [ ${{inputs.showall}} == 'yes' ];then showall="-a " ;else showall="" ;fi
+if [ ${{inputs.showdirectoryname}} == 'yes' ];then showdirectoryname="-d " ;else showdirectoryname="" ;fi
+if [ ${{inputs.showchangetime}} == 'yes' ];then showchangetime="-D " ;else showchangetime="" ;fi
+if [ ${{inputs.showsize}} == 'yes' ];then showsize="-s " ;else showsize="" ;fi
+if [ ${{inputs.showallname}} == 'yes' ];then showallname="-f " ;else showallname="" ;fi
+if [ ${{inputs.ignoregit}} == 'yes' ];then tree ${showall}${showdirectoryname}${showchangetime}${showsize}${showallname}-I ".git" > ./tree.bak ;else tree ${showall}${showdirectoryname}${showchangetime}${showsize}${showallname} > ./tree.bak ;fi
+        
+echo "tree ${showall}${showdirectoryname}${showchangetime}${showsize}${showallname}${ignoregit} > ./tree.bak"
